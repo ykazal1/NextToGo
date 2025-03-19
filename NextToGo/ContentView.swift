e5b51dfbc0196efb5b323eecdb434384111fbe44
YK <yacob.kazal@qantas.com.au>
2025-03-20 02:15:16 +1100

    Initial Commit

---
+++ NextToGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextToGo
+//
+//  Created by yacob kazal on 20/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by yacob kazal on 20/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
